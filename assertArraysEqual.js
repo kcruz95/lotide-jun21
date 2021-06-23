83daae5031461dba34d688a20a36894aa9dfe32f
kcruz95 <kyle.cruz@hotmail.ca>
2021-06-23 01:34:25 -0700

    assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,17 @@
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne !== arrTwo) {
+    return false;
+  } for (let i = 0; i < arrOne.length; i = i + 1) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArray((arrayOne, arrayTwo))) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
