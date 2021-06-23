9c3fcd216a0ea53c9eb82a7f7bb0640c6320dd0e
kcruz95 <kyle.cruz@hotmail.ca>
2021-06-23 00:31:34 -0700

    eqArrays.js code review tomorrow

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne !== arrTwo) {
+    return false;
+  } for (let i = 0; i < arrOne.length; i = i + 1) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  } return true;
+};
+
+// TEST CODE
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
