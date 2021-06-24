27181ee41fe5e71790e1e12247f9d59b4d535749
kcruz95 <kyle.cruz@hotmail.ca>
2021-06-23 22:03:46 -0700

    letterPositions

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const eqArrays = function(arrOne, arrTwo) {
+  if (arrOne !== arrTwo) {
+    return false;
+  } for (let i = 0; i < arrOne.length; i = i + 1) {
+    if (arrOne[i] !== arrTwo[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArray((arrayOne, arrayTwo))) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (sentence) {
+  const count = {};
+  for( let i = 0; i < sentence.length; i++) {
+    console.log(sentence[i]);
+    if (sentence[i] !== " ") {
+      console.log(count[sentence[i]]);
+      if (count[sentence[i]] === undefined) {
+        count[sentence[i]] = [i];
+      } else {
+        count[sentence[i]].push(i);
+      }
+    }
+  }
+  console.log(count);
+};
+
+// countLetters("hello");
+// assertArraysEqual(letterPositions("hello").e, [1]);
+// .push()
+countLetters("lighthouse in the house");
+// countLetters("light lig");
