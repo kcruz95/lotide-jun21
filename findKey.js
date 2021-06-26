d71026e9a1f2cf7d3465d064f5ce89c2d0cdb791
kcruz95 <kyle.cruz@hotmail.ca>
2021-06-26 16:59:34 -0700

    findKey.js

---
+++ findKey.js
@@ -0,0 +1,37 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (obj, callback) => {
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+
+  let(callback = findKey("noma"));
+  if (callback) {
+    console.log(`${findKey} found!`)
+  } else {
+    console.log(undefined);
+  }
+};
+// console.log(callback);
+
+// TEST CODE
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+// console.log(key);
+// console.log(obj);
+// console.log(findKey);
