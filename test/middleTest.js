31972ea7a9b146a9381da7016822975f94221bf4
kcruz95 <kyle.cruz@hotmail.ca>
2021-06-28 22:23:21 -0700

    middleTest.js && tailTest.js work

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,21 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-console.log(assertArraysEqual(middle([1])))
+
+describe("#middle", () => {
+  it("should return [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("should return [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("should return [2, 3] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+});
+
 // TEST CODE
 
 // console.log('first case', middle([2, 4]));
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,14 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-// TEST CASES
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+  it("returns 3 for words.length", () => {
+    assert.strictEqual((words.length), 3);
+  });
+});
+
+// // TEST CASES
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
